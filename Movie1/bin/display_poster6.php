a99197bb75cc0516a4ffbe6222e7f8da8d25adc6
saikiran <devineni.saikiran@ltts.com>
2020-11-09 09:58:22 +0530

    updated

---
+++ Movie1/bin/display_poster6.php
@@ -0,0 +1,19 @@
+<?php
+
+ $MovieName ='Aravidha Sametha';
+  //do some validation h'ere to ensure id is safe
+
+  $link = mysqli_connect("localhost","root","","MovieMagic1");
+
+
+  $result = mysqli_query($link,"SELECT * FROM movies WHERE MovieName= '$MovieName'");
+  $row = mysqli_fetch_assoc($result);
+  $s=$row['img'];
+//header("Content-type: image/jpeg");
+ //echo $s.'< alt="HTML5 Icon" style="width:128px;height:128px">';
+
+
+ header("Content-type: image/jpeg");
+ echo $s. '<alt="HTML5 Icon" style="width:450x;height:415px">';
+
+?>
